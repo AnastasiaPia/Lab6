e1e1e99b347004e8f312b83431b5d8655c3fc140
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-10-16 20:58:17 +0200

    tests greedy and brute force

--- tests/testthat/test_brute_force_knapsack.R
+++ tests/testthat/test_brute_force_knapsack.R
@@ -8,72 +8,42 @@ test_check("Lab6")
 
 
 
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <- data.frame(
+  w=sample(1:4000, size = n, replace = TRUE),
+  v=runif(n = n, 0, 10000)
+)
 
-# Load the brute_force_knapsack function
-source("bruteForce.R")
-
-# Test 1: Test with a simple data frame
-test_that("Test with a simple data frame", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, 30))
-  result <- brute_force_knapsack(x, 5)
-  expect_equal(result$value, 40)
-  expect_equal(result$elements, c(2, 3))
+test_that("Correct object is returned", {
+  expect_silent(bfk <- brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
+  expect_named(bfk, c("value", "elements"))
 })
 
-# Test 2: Test with empty data frame
-test_that("Test with an empty data frame", {
-  x <- data.frame(w = numeric(0), v = numeric(0))
-  result <- brute_force_knapsack(x, 5)
-  expect_equal(result$value, 0)
-  expect_equal(result$elements, numeric(0))
+
+test_that("functions rejects errounous input.", {
+  expect_error(brute_force_knapsack("hej", 3500))
+  expect_error(brute_force_knapsack(x = knapsack_objects[1:8,], W = -3500))
 })
 
-# Test 3: Test with negative weight
-test_that("Test with negative weight", {
-  x <- data.frame(w = c(2, 3, -4), v = c(10, 20, 30))
-  expect_error(brute_force_knapsack(x, 5), "W must be a positive numeric value.")
-})
+test_that("Function return correct results.", {
+  bfk <- brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+  expect_equal(round(bfk$value), 16770)
+  expect_true(all(round(bfk$elements) %in% c(5, 8)))
 
-# Test 4: Test with non-numeric data frame
-test_that("Test with non-numeric data frame", {
-  x <- data.frame(w = c(2, 3, 4), v = c("a", "b", "c"))
-  expect_error(brute_force_knapsack(x, 5), "The x must be a data frame.")
-})
+  bfk <- brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
+  expect_equal(round(bfk$value), 16770)
+  expect_true(all(round(bfk$elements) %in% c(5, 8)))
 
-# Test 5: Test with no 'w' or 'v' columns
-test_that("Test with no 'w' or 'v' columns", {
-  x <- data.frame(x = c(2, 3, 4), y = c(10, 20, 30))
-  expect_error(brute_force_knapsack(x, 5), "The x must be a data frame with column names 'w' and 'v'.")
-})
+  bfk <- brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
+  expect_equal(round(bfk$value), 15428)
+  expect_true(all(round(bfk$elements) %in% c(3, 8)))
 
-# Test 6: Test with negative values in 'x'
-test_that("Test with negative values in 'x'", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, -30))
-  expect_error(brute_force_knapsack(x, 5), "The x must be a data frame with only positive values.")
-})
+  bfk <- brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
+  expect_equal(round(bfk$value), 15428)
+  expect_true(all(round(bfk$elements) %in% c(3, 8)))
 
-# Test 7: Test with parallel computation
-test_that("Test with parallel computation", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, 30))
-  result <- brute_force_knapsack(x, 5, parallel = TRUE)
-  expect_equal(result$value, 40)
-  expect_equal(result$elements, c(2, 3))
-})
-
-# Test 8: Test with parallel computation and negative weight
-test_that("Test with parallel computation and negative weight", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, 30))
-  expect_error(brute_force_knapsack(x, -5, parallel = TRUE), "W must be a positive numeric value.")
-})
-
-# Test 9: Test with parallel computation and non-numeric data frame
-test_that("Test with parallel computation and non-numeric data frame", {
-  x <- data.frame(w = c(2, 3, 4), v = c("a", "b", "c"))
-  expect_error(brute_force_knapsack(x, 5, parallel = TRUE), "The x must be a data frame.")
-})
-
-# Test 10: Test with parallel computation and negative values in 'x'
-test_that("Test with parallel computation and negative values in 'x'", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, -30))
-  expect_error(brute_force_knapsack(x, 5, parallel = TRUE), "The x must be a data frame with only positive values.")
+  st <- system.time(bfk <- brute_force_knapsack(x = knapsack_objects[1:16,], W = 2000))
+  expect_true(as.numeric(st)[2] >= 0.00)
 })
--- tests/testthat/test_greedy.R
+++ tests/testthat/test_greedy.R
@@ -3,74 +3,47 @@ library(testthat)
 # Load the greedy_knapsack function
 source("greedy.R")
 
-# Test 1: Test with a simple data frame
-test_that("Test with a simple data frame", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, 30))
-  result <- greedy_knapsack(x, 5)
-  expect_equal(result$value, 50)
-  expect_equal(result$elements, c(3, 2, 1))
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <- data.frame(
+  w=sample(1:4000, size = n, replace = TRUE),
+  v=runif(n = n, 0, 10000)
+)
+
+test_that("Correct object is returned", {
+  expect_silent(gk <- greedy_knapsack(x = knapsack_objects[1:8,], W = 3500))
+  expect_named(gk, c("value", "elements"))
 })
 
-# Test 2: Test with empty data frame
-test_that("Test with an empty data frame", {
-  x <- data.frame(w = numeric(0), v = numeric(0))
-  result <- greedy_knapsack(x, 5)
-  expect_equal(result$value, 0)
-  expect_equal(result$elements, numeric(0))
+test_that("functions rejects errounous input.", {
+  expect_error(greedy_knapsack("hej", 3500))
+  expect_error(greedy_knapsack(x = knapsack_objects[1:8,], W = -3500))
 })
 
-# Test 3: Test with negative weight
-test_that("Test with negative weight", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, 30))
-  expect_error(greedy_knapsack(x, -5), "W must be a positive number.")
-})
+test_that("Function return correct results.", {
+  gk <- greedy_knapsack(x = knapsack_objects[1:8,], W = 3500)
+  expect_equal(round(gk$value), 15428)
+  expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-# Test 4: Test with non-numeric data frame
-test_that("Test with non-numeric data frame", {
-  x <- data.frame(w = c(2, 3, 4), v = c("a", "b", "c"))
-  expect_error(greedy_knapsack(x, 5), "You must provide a data frame.")
-})
+  gk <- greedy_knapsack(x = knapsack_objects[1:12,], W = 3500)
+  expect_equal(round(gk$value), 15428)
+  expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-# Test 5: Test with no 'w' or 'v' columns
-test_that("Test with no 'w' or 'v' columns", {
-  x <- data.frame(x = c(2, 3, 4), y = c(10, 20, 30))
-  expect_error(greedy_knapsack(x, 5), "The data frame must have columns 'w' and 'v'.")
-})
+  gk <- greedy_knapsack(x = knapsack_objects[1:8,], W = 2000)
+  expect_equal(round(gk$value), 15428)
+  expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-# Test 6: Test with negative values in 'x'
-test_that("Test with negative values in 'x'", {
-  x <- data.frame(w = c(2, 3, 4), v = c(10, 20, -30))
-  expect_error(greedy_knapsack(x, 5), "All values in x must be non-negative.")
-})
+  gk <- greedy_knapsack(x = knapsack_objects[1:12,], W = 2000)
+  expect_equal(round(gk$value), 15428)
+  expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-# Test 7: Test with a large data frame
-test_that("Test with a large data frame", {
-  x <- data.frame(w = 1:50, v = 51:100)
-  result <- greedy_knapsack(x, 200)
-  expect_equal(result$value, 2550)
-  expect_equal(result$elements, 1:50)
-})
+  st <- system.time(gk <- greedy_knapsack(x = knapsack_objects[1:16,], W = 2000))
+  expect_true(as.numeric(st)[2] <= 0.01)
 
-# Test 8: Test with a large weight limit
-test_that("Test with a large weight limit", {
-  x <- data.frame(w = 1:10, v = 11:20)
-  result <- greedy_knapsack(x, 1000)
-  expect_equal(result$value, 155)
-  expect_equal(result$elements, 1:10)
-})
+  gk <- greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+  expect_equal(round(gk$value), 192647)
 
-# Test 9: Test with the smallest possible weight limit
-test_that("Test with the smallest possible weight limit", {
-  x <- data.frame(w = 1:10, v = 11:20)
-  result <- greedy_knapsack(x, 1)
-  expect_equal(result$value, 0)
-  expect_equal(result$elements, numeric(0))
-})
-
-# Test 10: Test with no items fit in the knapsack
-test_that("Test with no items fit in the knapsack", {
-  x <- data.frame(w = c(6, 7, 8), v = c(10, 20, 30))
-  result <- greedy_knapsack(x, 5)
-  expect_equal(result$value, 0)
-  expect_equal(result$elements, numeric(0))
+  gk <- greedy_knapsack(x = knapsack_objects[1:1200,], W = 3500)
+  expect_equal(round(gk$value), 270290)
 })
