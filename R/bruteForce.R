13b957b1cacce2919df6b4ac07f024f7ac8965e7
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-10-18 00:07:13 +0200

    adding comments to the code

--- R/bruteForce.R
+++ R/bruteForce.R
@@ -24,57 +24,57 @@
 
 brute_force_knapsack<-function(x, W, parallel = FALSE) {  #parallel is to show the parallel computation
   if (!is.data.frame(x)) {
-    stop("The x must be a data frame.")
+    stop("The x must be a data frame.")                   #checks if the argument is a data frame
   }
   if (!is.numeric(W) || W <= 0) {
-    stop("W must be a positive numeric value.")
+    stop("W must be a positive numeric value.")           #checks if the W is a numerical value
   }
-  if (!all(c("w", "v") %in% colnames(x))) {
+  if (!all(c("w", "v") %in% colnames(x))) {               #checks if the data frame has columns w,v
     stop("The x must be a data frame with column names 'w' and 'v'.")
   }
-  if (!all(x > 0, na.rm = TRUE)) {
+  if (!all(x > 0, na.rm = TRUE)) {                        #checks if the values in the data frame are positive
     stop("The x must be a data frame with only positive values.")
   }
-  x <- x[x$w <= W, ]
-  n <- nrow(x)
-  big_o<- 2 ^ n - 1    #the number of combinations of items 2^n-1 in the knapsack problem
-  if (parallel) {
-    cores <- parallel::detectCores()
-    cl <- parallel::makeCluster(cores)
-    combinations <- parallel::parLapply(cl, 1:big_o, function(x) {
+  x <- x[x$w <= W, ]                                                       #filters the rows of the x data frame, retaining only those with weight ('w') less than or equal to the knapsack capacity W.
+  n <- nrow(x)                                                             #n is assigned the number of rows in the filtered x data frame, which represents the number of available items.
+  big_o<- 2 ^ n - 1                                                        #the number of possible combinations of items 2^n-1 that can be included in the knapsack problem
+  if (parallel) {                                                          #If parallel is set to TRUE, it enters the parallel computation section.
+    cores <- parallel::detectCores()                                       #It detects the number of CPU cores available for parallel processing
+    cl <- parallel::makeCluster(cores)                                     #A cluster of workers for parallel computation is created
+    combinations <- parallel::parLapply(cl, 1:big_o, function(x) {         #The combinations variable is populated with all possible combinations of items. It uses parallel::parLapply to distribute the combinations across the worker nodes
       as.integer(head(intToBits(x), n))
     })
-    weight <- unlist(lapply(combinations, function(combin) {
+    weight <- unlist(lapply(combinations, function(combin) {               #weight is calculated for each combination of items, and a list of these weights is obtained.
       sum(x$w[as.logical(combin)])
     }))
 
     n <- length(combinations)
-    values <- numeric(n)
+    values <- numeric(n)                                                   #values is initialized as an empty numeric vector.
 
-    for (i in 1:n) {
+    for (i in 1:n) {                                                       #It iterates through the combinations, calculates the total value for each combination, and stores it in the values vector.
       subset_values <- x$v[as.logical(combinations[[i]])]
       values[i] <- sum(subset_values)
     }
 
-    i <- which.max(value)
-    value <- round(value[i])
-    elements <- as.integer(rownames(x[as.logical(combinations[[i]]), ]))
-    parallel::stopCluster(cl)
+    i <- which.max(value)                                                  #The index i of the combination with the maximum value is determined using which.max.
+    value <- round(value[i])                                               #The maximum value is rounded and assigned to the value variable
+    elements <- as.integer(rownames(x[as.logical(combinations[[i]]), ]))   #The elements (items) included in the knapsack are extracted based on the chosen combination and stored in the elements variable.
+    parallel::stopCluster(cl)                                              #The cluster of workers is stopped using parallel::stopCluster
     return(list(value = value, elements = elements))
-  } else {
-    combinations <- matrix(nrow = big_o, ncol = n)
+  } else {                                                                 #If parallel is set to FALSE, it enters the non-parallel computation section.
+    combinations <- matrix(nrow = big_o, ncol = n)                         #The combinations matrix is initialized to store all possible combinations.
     for (i in 1:big_o) {
-      combinations[i, ] <- as.integer(head(intToBits(i), n))
+      combinations[i, ] <- as.integer(head(intToBits(i), n))               #It iterates through the combinations and calculates each combination using bitwise operations (intToBits).
     }
-    weight <- sapply(1:big_o, function(i) sum(x$w[as.logical(combinations[i, ])])
+    weight <- sapply(1:big_o, function(i) sum(x$w[as.logical(combinations[i, ])])  #weight is calculated for each combination of items, and a list of these weights is obtained.
     )
-    combinations <- combinations[weight <= W, ]
-    value <- sapply(1:nrow(combinations), function(i) sum(x$v[as.logical(combinations[i, ])])
-    )
-    i <- which.max(value)
-    value <- round(value[i])
-    elements <- as.integer(rownames(x[as.logical(combinations[i, ]), ]))
-    return(list(value = value, elements = elements))
+    combinations <- combinations[weight <= W, ]                                    #The combinations matrix is filtered to keep only combinations where the total weight does not exceed the knapsack capacity.
+    value <- sapply(1:nrow(combinations), function(i) sum(x$v[as.logical(combinations[i, ])])   #The value variable is initialized as an empty numeric vector.
+    )                                                                                           #Thw value variable iterates through the filtered combinations and calculates the total value for each combination, storing it in the value vector.
+    i <- which.max(value)                                                  #The index i of the combination with the maximum value is determined using which.max.
+    value <- round(value[i])                                               #The maximum value is rounded and assigned to the value variable.
+    elements <- as.integer(rownames(x[as.logical(combinations[i, ]), ]))   #The elements (items) included in the knapsack are extracted based on the chosen combination and stored in the elements variable.
+    return(list(value = value, elements = elements))                       #the function returns a list containing the maximum value and the indices of the selected elements in the knapsack.
   }
 }
 
--- R/dynamic.R
+++ R/dynamic.R
@@ -1,7 +1,7 @@
 #'Dynamic Knapsack
 #'
 #'@description
-#'This function solves the 0/1 knapsack problem using dynamic programming.
+#'This function solves the 0/1 knapsack problem using dynamic programming. The knapsack problem is a classic optimization problem where you must choose a subset of items with given weights and values to maximize the total value while not exceeding a given weight limit.
 #'
 #'@param x A data frame with two columns, 'v' for values and 'w' for weights.
 #'@param W The capacity of the knapsack.
@@ -25,27 +25,27 @@
 
 
 
-knapsack_dynamic <- function(x, W) {
-  if (!is.data.frame(x) || !is.numeric(x$w) || !is.numeric(x$v)) {
+knapsack_dynamic <- function(x, W) {                                            #R function that takes two arguments: x (a data frame with item information) and W (the weight capacity of the knapsack).
+  if (!is.data.frame(x) || !is.numeric(x$w) || !is.numeric(x$v)) {              #It checks if x is a data frame and if it has the required columns 'w' and 'v'. If not, the function raises an error with the message "The x must be a data frame with column names 'w' and 'v'."
     stop("The x must be a data frame with column names 'w' and 'v'.")
   }
 
-  if (!is.numeric(W)) {
+  if (!is.numeric(W)) {                                                         #It checks if W is a numeric value. If not, the function raises an error with the message "W must be a number."
     stop("W must be a number.")
   }
-  if (W <= 0) {
+  if (W <= 0) {                                                                 #checks if W is positive. If W is not greater than 0, the function raises an error with the message "W must be positive."
     stop("W must be positive.")
   }
-  if (!all(colnames(x) %in% c("w", "v"))) {
+  if (!all(colnames(x) %in% c("w", "v"))) {                                     #checks if all column names in the x data frame are either 'w' or 'v'. If not, the function raises an error with the message "The x must be a data frame with column names 'w' and 'v'."
     stop("The x must be a data frame with column names 'w' and 'v'.")
   }
-  if (!all(x > 0, na.rm = TRUE)) {
+  if (!all(x > 0, na.rm = TRUE)) {                                              #It checks if all values in the x data frame are positive, including the weights and values. If any value is non-positive (including missing values, NA), the function raises an error with the message "You must provide positive values for x."
     stop("You must provide positive values for x.")
   }
-  if (nrow(x) == 0) {
+  if (nrow(x) == 0) {                                                           #If there are no rows in the x data frame (i.e., nrow(x) is 0), the function returns a list containing 0 as the maximum value and an empty numeric vector as the list of selected elements, since there are no elements to select.
     return(list(0, numeric(0)))
   }
-  for (i in x$w) {
+  for (i in x$w) {                                                              #Two for loops are used to check that all weights (w) in the data frame are positive integers. It uses the modulo operator (%%) to check if the weights are integers. If any weight is not an integer, the function raises an error.
     if (i %% 1 != 0)
       stop("weights must be positive integers")
   }
@@ -53,17 +53,17 @@ knapsack_dynamic <- function(x, W) {
     if(i %% 1 != 0)
       stop("weights must be positive integers")
   }
-  n <- nrow(x)
-  max_value <- matrix(0, nrow = n+1, ncol = W+1)
-  elem_list <- list()
-  for(i in 1:(n+1)*(W+1))
+  n <- nrow(x)                                                                  #The n variable is assigned the number of rows in the data frame x, which represents the number of available items
+  max_value <- matrix(0, nrow = n+1, ncol = W+1)                                #A max_value matrix is initialized with all elements set to 0. This matrix will be used to store the maximum values that can be achieved for different combinations of items and weight limits.
+  elem_list <- list()                                                           #An empty list, elem_list, is initialized. This list will be used to keep track of the selected elements in the knapsack.
+  for(i in 1:(n+1)*(W+1))                                                       #Nested for loops iterate through item indices (i) and weight limits (j) to calculate the maximum value that can be achieved for various combinations of items and weight limits.
     elem_list[i] <- list(NULL)
     dim(elem_list) <- c(n+1, W+1)
   for(i in 2:n+1) {
-    item_value <- x$v[i-1]
-    item_weight <- x$w[i-1]
+    item_value <- x$v[i-1]                                                      #item_value is assigned the value of the current item (indexed by i-1) from the 'v' column.
+    item_weight <- x$w[i-1]                                                     #item_weight is assigned the weight of the current item (indexed by i-1) from the 'w' column
     for(j in 2:W+1) {
-      if(item_weight > j-1) {
+      if(item_weight > j-1) {                                                   #checks whether the weight of the current item exceeds the current weight limit (j-1). If it does, it means that the item cannot be included in the knapsack, so the maximum value is the same as the maximum value achieved without this item.
         max_value[i,j] <- max_value[i-1,j]
         if(is.null(elem_list[[i-1,j]]))
           elem_list[[i,j]] <- list(NULL)
@@ -71,9 +71,9 @@ knapsack_dynamic <- function(x, W) {
           elem_list[[i,j]] <- elem_list[[i-1,j]]
       }
       else {
-        max_value[i,j] <- max(max_value[i-1, j], item_value + max_value[i-1, j-item_weight])
+        max_value[i,j] <- max(max_value[i-1, j], item_value + max_value[i-1, j-item_weight])   #If the item's weight is less than or equal to the current weight limit, the algorithm compares the maximum value achieved without the item  max_value[i-1, j]and the maximum value achieved by including the item item_value + max_value[i-1, j-item_weight]
         if(max_value[i-1, j] < item_value + max_value[i-1, j-item_weight])
-          elem_list[[i, j]] <- c(elem_list[[i-1, j-item_weight]], i-1)
+          elem_list[[i, j]] <- c(elem_list[[i-1, j-item_weight]], i-1)                    #Depending on which value is larger, the corresponding elements (selected items) are updated in elem_list.
         else {
           if(is.null(elem_list[[i-1,j]]))
             elem_list[[i,j]] <- list(NULL)
@@ -83,9 +83,9 @@ knapsack_dynamic <- function(x, W) {
       }
     }
   }
-  elem_list <- unlist(elem_list[[n+1, W+1]])
-  result <- list(round(max_value[n+1, W+1]), elem_list)
-  return(result)
+  elem_list <- unlist(elem_list[[n+1, W+1]])                                    #The elem_list at the end of the process contains the indices of the selected items that maximize the knapsack's value.
+  result <- list(round(max_value[n+1, W+1]), elem_list)                         #The result is stored in a list containing the rounded maximum value and the list of selected elements (items).
+  return(result)                                                                #The function returns this result.
 }
 
 
