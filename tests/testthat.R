2d719ad4cff8ac558a4e8ebc06c452d0487c5f09
NAZLIBILGIC <nazli.blgc@gmail.com>
2023-10-17 13:34:31 +0200

    test fix3

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-#library(testthat)
-#library(Lab6)
+library(testthat)
+library(Lab6)
 
 test_check("Lab6")
