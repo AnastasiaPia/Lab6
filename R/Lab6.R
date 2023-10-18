57f94ce08ac39462a4853b839ba7d0eb03e5a9c9
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-10-18 13:50:25 +0200

    adding package documentation

---
+++ R/Lab6.R
@@ -0,0 +1,17 @@
+#'Lab6:: A package for implementing solutions for the knapsack problem
+#'
+#'@section Brute Force:
+#'Brute force method is used to see the execution time of the problem.
+#'
+#'@section Dynamic knapsack function:
+#'Dynamic method is used to see the execution time of the problem.
+#'
+#'@section Greedy knapsack:
+#'Greedy method is used to see the execution time of the problem.
+#'
+#'@description
+#'With three different methods execution times calculated and compared. Also we profile our code with parallel method and discuss the results about the gained time.
+#'
+#'@docType package
+#'@name Lab6
+NULL
